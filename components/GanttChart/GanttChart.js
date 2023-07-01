449aa52d2daf7157b17bd426deab11048ffa3c41
shigeo.shinohara <s.shinohara@sync-web.jp>
2023-07-02 01:51:17 +0900

    GanttChartへstateを追加

--- components/GanttChart/GanttChart.js
+++ components/GanttChart/GanttChart.js
@@ -1,4 +1,28 @@
+import { useState, useEffect } from 'react';
+import { client } from '../../utils/fetchWrapper';
+import { error } from 'next/dist/build/output/log';
 export default function GanttChart() {
+  const [tasks, setTasks] = useState(null);
+  const [taskDurations, setTaskDurations] = useState(null);
+  const [timeRange, setTimeRange] = useState({
+    fromSelectMouth: 0,
+    fromSelectYear: '2023',
+    toSelectMouth: 1,
+    toSelectYear: '2023',
+  });
+
+  useEffect(() => {
+    client('data.json').then(
+      (data) => {
+        setTasks(data?.tasks);
+        setTaskDurations(data?.taskDurations);
+      },
+      (error) => {
+        console.error('Error: ', error);
+      }
+    );
+  }, []);
+
   return (
     <div id="gantt-container">
       <style jsx>{`
