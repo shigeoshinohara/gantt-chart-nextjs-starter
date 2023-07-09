0241e19766f922a1289211954c2ef5924cc9bf99
shigeo.shinohara <s.shinohara@sync-web.jp>
2023-07-10 00:41:58 +0900

    タスク追加処理の実装

--- components/GanttChart/AddTask.js
+++ components/GanttChart/AddTask.js
@@ -10,6 +10,21 @@ export default function AddTask({ setTasks }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    setTasks((prevState) => {
+      const newState = prevState;
+      // find largest task number, add 1 for new task - else could end up with tasks with same id
+      const maxIdVal = prevState.reduce(function (a, b) {
+        return Math.max(a, b.id);
+      }, -Infinity);
+
+      // create new task
+      newState.push({
+        id: isFinite(maxIdVal) ? maxIdVal + 1 : 1,
+        name: task,
+      });
+
+      return [...newState];
+    });
     setTask('');
   }
 
